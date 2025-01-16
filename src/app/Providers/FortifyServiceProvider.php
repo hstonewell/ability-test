a6f9dd48ac1a1b997f768a0076ad9cefc3709c3d
haruna ishii <eijen.6@gmail.com>
2025-01-16 23:12:35 +0200

    Update: Comment off unused class and code

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -3,31 +3,23 @@
 namespace App\Providers;
 
 use App\Actions\Fortify\CreateNewUser;
-use App\Actions\Fortify\ResetUserPassword;
-use App\Actions\Fortify\UpdateUserPassword;
-use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
-use Laravel\Fortify\Contracts\LogoutResponse;
+use Laravel\Fortify\Http\Requests\LoginRequest as FortifyLoginRequest;
+use App\Http\Requests\LoginRequest;
+
 
 class FortifyServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register()
+    public function register(): void
     {
-        $this->app->instance(LogoutResponse::class, new class implements LogoutResponse
-        {
-            public function toResponse($request)
-            {
-                return redirect('/login');
-            }
-        });
+    //
     }
 
     /**
@@ -37,14 +29,6 @@ class FortifyServiceProvider extends ServiceProvider
     {
 
         Fortify::createUsersUsing(CreateNewUser::class);
-        // Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        // Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        // Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-
-        RateLimiter::for('login', function (Request $request) {
-            $email = (string) $request->email;
-            return Limit::perMinute(10)->by($email . $request->ip());
-        });
 
         Fortify::registerView(function () {
             return view('auth.register');
@@ -54,8 +38,12 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.login');
         });
 
-        // RateLimiter::for('two-factor', function (Request $request) {
-        //    return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        // });
+        RateLimiter::for('login', function (Request $request) {
+            $email = (string) $request->email;
+
+            return Limit::perMinute(10)->by($email . $request->ip());
+        });
+
+        $this->app->bind(FortifyLoginRequest::class, LoginRequest::class);
     }
 }
