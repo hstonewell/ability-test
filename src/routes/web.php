a560b5e678184decfe7a35e203a710dd941e48bd
haruna ishii <eijen.6@gmail.com>
2025-01-16 23:17:12 +0200

    Updated: Move some routes into the middleware group

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,36 +2,18 @@
 
 
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\AdminController;
-use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 use Tests\TestCase;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [ContactController::class, 'index'])->name('index');
 Route::post('/confirm', [ContactController::class, 'confirm'])->name('confirm');
 Route::post('/thanks', [ContactController::class, 'store'])->name('store');
 Route::get('/thanks', [ContactController::class, 'thanks'])->name('thanks');
 
-Route::get('/register', [AdminController::class, 'register']);
-Route::post('/register', [AdminController::class, 'store']);
-
-Route::get('/login', [AdminController::class, 'login']);
-
 Route::middleware('auth')->group(function () {
-    Route::get('/admin', [AdminController::class, 'admin'])->name('admin');
+    Route::get('/admin', [ContactController::class, 'admin']);
+    Route::get('/search', [ContactController::class, 'search']);
+    Route::post('/delete', [ContactController::class, 'destroy']);
+    Route::post('/export', [ContactController::class, 'export']);
 });
-
-//Route::get('/admin', [AdminController::class, 'admin']);
-Route::get('/admin/search', [AdminController::class, 'search']);
-Route::delete('/admin/delete', [AdminController::class, 'destroy']);
