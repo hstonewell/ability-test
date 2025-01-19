caf7268724c336a302cefb597d34fde614ce2434
haruna ishii <eijen.6@gmail.com>
2025-01-19 18:50:55 +0200

    modify: routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,19 +1,23 @@
 <?php
 
-
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
-use Tests\TestCase;
-
 
 Route::get('/', [ContactController::class, 'index'])->name('index');
+
 Route::post('/confirm', [ContactController::class, 'confirm'])->name('confirm');
-Route::post('/thanks', [ContactController::class, 'store'])->name('store');
+Route::post('/thanks', [ContactController::class, 'store'])->name('contact.store');
 Route::get('/thanks', [ContactController::class, 'thanks'])->name('thanks');
 
+Route::get('/login', [AdminController::class, 'login'])->name('login');
+Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store'])->name('store');
+
 Route::middleware('auth')->group(function () {
-    Route::get('/admin', [ContactController::class, 'admin']);
-    Route::get('/search', [ContactController::class, 'search']);
-    Route::post('/delete', [ContactController::class, 'destroy']);
-    Route::post('/export', [ContactController::class, 'export']);
+    Route::get('/admin', [AdminController::class, 'admin'])->name('admin');
+    Route::get('/search', [AdminController::class, 'search'])->name('search');
+    Route::post('/delete', [AdminController::class, 'destroy'])->name('destroy');
+    Route::post('/export', [AdminController::class, 'export']);
 });
