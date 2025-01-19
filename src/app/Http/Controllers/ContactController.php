ef3289044f1102d98311891ae32065daf7b43b08
haruna ishii <eijen.6@gmail.com>
2025-01-19 18:54:32 +0200

    Remove: unused actions

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -10,17 +10,18 @@ use Carbon\Carbon;
 
 class ContactController extends Controller
 {
+    // 問い合わせフォーム表示
     public function index()
     {
         $categories = Category::all();
         return view('index', compact('categories'));
     }
 
+    // 確認ページ表示
     public function confirm(ContactRequest $request)
     {
         $contact = $request->validated();
         $category = Category::find($contact['category_id']);
-
 
         return view('/confirm', compact('contact', 'category'));
     }
@@ -53,34 +54,6 @@ class ContactController extends Controller
         $categories = Category::all();
         $csvData = Contact::all();
         return view('admin', compact('contacts', 'categories', 'csvData'));
-    }
-
-    public function search(Request $request)
-    {
-        if ($request->has('reset')) {
-            $contacts = Contact::with('category')->paginate(7);
-        } else {
-            $date = $request->input('date');
-            $date = $date ? Carbon::parse($date)->toDateString() : null;
-
-            $contacts = Contact::with('category')
-            ->CategorySearch($request->input('category_id'))
-            ->GenderSearch($request->input('gender'))
-            ->byDate($date)
-                ->KeywordSearch($request->input('keyword'))
-                ->paginate(7)
-                ->appends($request->all());
-        }
-        $categories = Category::all();
-
-        return view('/auth/admin', compact('contacts', 'categories'));
-    }
-
-
-    public function destroy(Request $request)
-    {
-        Contact::find($request->id)->delete();
-        return redirect('/admin');
     }
 
 }
